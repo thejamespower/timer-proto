7eddcfa5f6f36a6062a0a0424672dffc714f1f59
James Power <thejamespower@gmail.com>
2018-11-17 11:07:03 +0000

    TimerDeleteButton render tests

--- src/components/TimerDeleteButton/TimerDeleteButton.test.jsx
+++ src/components/TimerDeleteButton/TimerDeleteButton.test.jsx
@@ -3,19 +3,55 @@ import renderer from 'react-test-renderer'
 import TimerDeleteButton from './TimerDeleteButton'
 
 describe('TimerDeleteButton', () => {
-  const props = {
-    active: false,
-    complete: false,
-    superTimerActive: false,
-    deleteTimer: () => null,
-    id: 'test',
-  }
+  let props
 
-  it('renders', () => {
-    const component = renderer.create(
-      <TimerDeleteButton {...props} />,
-    )
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
+  beforeEach(() => {
+    props = {
+      active: false,
+      complete: false,
+      superTimerActive: false,
+      deleteTimer: () => null,
+      id: 'test',
+    }
+  })
+
+  describe('render', () => {
+    it('renders default state', () => {
+      const component = renderer.create(
+        <TimerDeleteButton {...props} />,
+      )
+      const tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+
+    it('renders active state', () => {
+      props.active = true
+
+      const component = renderer.create(
+        <TimerDeleteButton {...props} />,
+      )
+      const tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+
+    it('renders complete state', () => {
+      props.complete = true
+
+      const component = renderer.create(
+        <TimerDeleteButton {...props} />,
+      )
+      const tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+
+    it('renders superTimerActive state', () => {
+      props.superTimerActive = true
+
+      const component = renderer.create(
+        <TimerDeleteButton {...props} />,
+      )
+      const tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+    })
   })
 })
