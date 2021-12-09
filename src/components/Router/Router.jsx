702e403023d139aecf375bef8c3a84161e9bc1ec
James Power <thejamespower@gmail.com>
2021-12-09 16:49:13 +0000

    Remove old route

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from '../../views/Home';
-import AddItem from '../../views/AddItem';
 
 const Router = () => (
   <BrowserRouter>
     <div>
       <Route path="/" exact component={Home} />
-      <Route path="/add-item" exact component={AddItem} />
     </div>
   </BrowserRouter>
 );
